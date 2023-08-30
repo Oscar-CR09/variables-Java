9c4b41b7047593c5ecb2ec2c49fc224d4ffabac3
Oscar <oscarcr189@outlook.com>
2023-08-29 23:02:02 -0600

    introduccion a las varaibles2

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -6,8 +6,21 @@ public class HolaMundo {
         System.out.println("saludar.ToUpperCase() = " + saludar.toLowerCase());
 
         int numero = 10;
+        boolean valor = true;
+        int numero2 = 5;
         System.out.println("numero = " + numero);
 
-        
+        var numero3 = "15";
+        String nombre;
+        nombre = "Oscar";
+        if (numero >10){
+            nombre = "Oscar";
+        }
+        System.out.println();
+                
+
+        int edad_persona = 5;
+        // comentarios
+
     }
 }
