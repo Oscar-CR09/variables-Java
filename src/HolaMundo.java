21f7908647d32fe1ba2db092c60172fc5d93b0a7
Oscar <oscarcr189@outlook.com>
2023-08-29 22:53:25 -0600

    introduccion a las varaibles

---
+++ src/HolaMundo.java
@@ -0,0 +1,13 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        String saludar ="Hola Mundo desde Java";
+        System.out.println(saludar);
+
+        System.out.println("saludar.ToUpperCase() = " + saludar.toLowerCase());
+
+        int numero = 10;
+        System.out.println("numero = " + numero);
+
+        
+    }
+}
