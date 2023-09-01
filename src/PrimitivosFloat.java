aa87202337247faa6f127574bebe2eeeed5a8213
Oscar <oscarcr189@outlook.com>
2023-08-31 22:10:11 -0600

    introduccion primitivos

---
+++ src/PrimitivosFloat.java
@@ -0,0 +1,8 @@
+public class PrimitivosFloat {
+    public static void main(String[] args) {
+
+        float realFloat = 1.5e4f;//1500f;
+        System.out.println("realFloat = " +realFloat);
+
+    }
+}
