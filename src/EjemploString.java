3aafa5159e3c8aab42345ebdd90c77b73833845d
Oscar <oscarcr189@outlook.com>
2023-09-03 18:50:27 -0600

    Introduccion a los String

---
+++ src/EjemploString.java
@@ -0,0 +1,19 @@
+public class EjemploString {
+    public static void main(String[] args) {
+        String curso = "Programacion Java";
+        String curso2= new String("programacion java");
+
+        boolean esIgual = curso ==curso2;
+        System.out.println("curso ==curso2 = " + esIgual);
+
+        esIgual = curso.equalsIgnoreCase(curso2);
+        System.out.println("curso.equalsIgnoreCase(curso2) = " + esIgual);
+
+        String curso3 = "Programacion Java";
+        esIgual = curso == curso3;
+
+        System.out.println("curso == curso3 = " + esIgual);
+
+    }
+
+}
