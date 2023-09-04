1679413f3552c0e3583f1746996d42139b808b7a
Oscar <oscarcr189@outlook.com>
2023-09-03 22:51:17 -0600

    test de rendimiento

---
+++ src/EjemploStringTestRendimientoConcat.java
@@ -0,0 +1,27 @@
+public class EjemploStringTestRendimientoConcat {
+    public static void main(String[] args) {
+
+        String a = "a";
+        String b = "b";
+        String c = a ;
+
+        StringBuilder sb = new StringBuilder(a);
+
+        long inicio = System.currentTimeMillis();
+
+        for (int i = 0; i < 500 ; i++){
+            // c= c.concat(a).concat(b).concat("\n"); // 500 = > 4 ms
+            // c += a + b + "\n"; // 500 => 39 ms
+            sb.append(a) // 500 => 0 ms
+                    .append(b)
+                    .append("\n");
+
+        }
+
+        long fin = System.currentTimeMillis();
+        System.out.println(fin - inicio );
+        System.out.println("c = " + c);
+        System.out.println("sb " + sb.toString());
+    }
+
+}
