9a095c3cf3056efbdf065544eb9a8f712c2d3d02
Oscar <oscarcr189@outlook.com>
2023-09-09 23:09:09 -0600

    anio bisiesto

--- src/SentenciasIfElseNumDiasMes.java
+++ src/SentenciasIfElseNumDiasMes.java
@@ -1,23 +1,29 @@
+import java.util.Scanner;
+
 public class SentenciasIfElseNumDiasMes {
     public static void main(String[] args) {
-        float promedio = 4.20f;
 
-        if (promedio>=6.5){
-            System.out.println("Felicitaciones, excelente promedio!");
-        }else if (promedio>=6.0){
-            System.out.println("Muy buen promedio!");
-        } else if (promedio >= 5.5) {
-            System.out.println("Buen promedio!");
-        } else if (promedio >=5.0) {
-            System.out.println("Regular, necesitas esforzarte un poco más!");
-        } else if (promedio >= 4.0) {
-            System.out.println("Insuficiente, necesitas estudiar mas!");
-        } else {
-            System.out.println("Reprobado!");
+        Scanner s =new Scanner(System.in);
+        System.out.println("Ingrese el numero del mes de 1-12");
+        int mes = s.nextInt();
+
+        int numerosDias = 0;
+
+        System.out.println("Ingrese el año (YYYY)");
+        int anio = s.nextInt();
+
+        if (mes == 1 || mes ==3 || mes ==5 || mes ==7 || mes ==8|| mes==10|| mes==12){
+            numerosDias= 31;
+
+        } else if (mes == 4|| mes == 6||mes==9||mes == 11) {
+            numerosDias=30;
+        }else if (mes == 2){
+            if (anio % 400 ==0 ||((anio%4==0)&&!(anio% 100 == 0))){
+                numerosDias =29;
+            }else {
+                numerosDias = 28;
+            }
         }
-
-        System.out.println("Tu promedio es " + promedio);
-
+        System.out.println("numerosDias = " + numerosDias);
     }
-
 }
