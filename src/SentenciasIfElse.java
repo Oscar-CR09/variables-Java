ebe370ad8f5a86c3dc71744e5d4ad8f2bb2e80ec
Oscar <oscarcr189@outlook.com>
2023-09-09 22:50:54 -0600

    Flujo de if else

---
+++ src/SentenciasIfElse.java
@@ -0,0 +1,23 @@
+public class SentenciasIfElse {
+    public static void main(String[] args) {
+        float promedio = 4.20f;
+
+        if (promedio>=6.5){
+            System.out.println("Felicitaciones, excelente promedio!");
+        }else if (promedio>=6.0){
+            System.out.println("Muy buen promedio!");
+        } else if (promedio >= 5.5) {
+            System.out.println("Buen promedio!");
+        } else if (promedio >=5.0) {
+            System.out.println("Regular, necesitas esforzarte un poco más!");
+        } else if (promedio >= 4.0) {
+            System.out.println("Insuficiente, necesitas estudiar mas!");
+        } else {
+            System.out.println("Reprobado!");
+        }
+
+        System.out.println("Tu promedio es " + promedio);
+
+    }
+
+}
