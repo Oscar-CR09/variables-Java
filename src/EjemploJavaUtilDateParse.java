4dc36522300ffd176c2e35790f23b87489a2fe41
Oscar <oscarcr189@outlook.com>
2023-09-18 23:15:16 -0600

    Comparar fechas

--- src/EjemploJavaUtilDateParse.java
+++ src/EjemploJavaUtilDateParse.java
@@ -15,6 +15,25 @@ public class EjemploJavaUtilDateParse {
             Date fecha = format.parse(s.next());
             System.out.println("fecha = " + fecha);
             System.out.println("format = " + format.format(fecha));
+
+            Date fecha2 = new Date();
+            System.out.println("fecha2 = " +fecha2);
+            if (fecha.after(fecha2)){
+                System.out.println("Fecha (del usuario) es despues que fecha2 ( actual) ");
+            }else if (fecha.before(fecha2)){
+                System.out.println("fecha es anterior que fecha2");
+            } else if (fecha.equals(fecha2)) {
+                System.out.println("fecha es igual a fecha2");
+            }
+
+            if (fecha.compareTo(fecha2) > 0 ){
+                System.out.println("Fecha (del usuario) es despues que fecha2 ( actual) ");
+
+            }else if (fecha.compareTo(fecha2)< 0 ){
+                System.out.println("fecha es anterior que fecha2");
+            } else if (fecha.compareTo(fecha2)==0) {
+                System.out.println("fecha es igual a fecha2");
+            }
         } catch (ParseException e) {
             e.printStackTrace();
         }
