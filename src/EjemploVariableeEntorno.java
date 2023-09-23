efe53dd46c4f9459f17c403b7c92408eae84cacf
Oscar <oscarcr189@outlook.com>
2023-09-22 23:22:08 -0600

    propiedades del sistema 3

---
+++ src/EjemploVariableeEntorno.java
@@ -0,0 +1,26 @@
+import javax.sound.midi.Soundbank;
+import java.util.Map;
+
+public class EjemploVariableeEntorno {
+    public static void main(String[] args) {
+
+        Map<String, String> varEnv = System.getenv();
+        System.out.println("Variables de ambiente delssitema = "  + varEnv);
+
+        String username =System.getenv("USERNAME");
+        System.out.println("username = " + username);
+
+        String javaHome =System.getenv("JAVA_HOME");
+        System.out.println("javaHome = " +javaHome);
+
+        String tenDir =System.getenv("TEMP");
+        System.out.println("tenDir = " + tenDir);
+
+        String path =System.getenv("Path");
+        System.out.println("path = " + path);
+
+        String path2 =varEnv.get("Path");
+        System.out.println("path2 = " + path2);
+        
+    }
+}
